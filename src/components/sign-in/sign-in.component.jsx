5367888261daae6e8a9937d44b184e8d7663f840
Daniel <squeakycamp@gmail.com>
2021-08-12 14:33:05 +0200

    adding the sign in and sign up page and added the sign in component, created form input and custom button components

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+    /*if you want to dynamically set the state in react, you need to put brackets around the key, 
+    so react knows that is supposed to be a dynamically set variable and not an actual value in the state. For the value of the key,
+    you dont need brackets around the variable because the value in the state is always a string or number or something like that,
+    so if you put in a variable like this, react knows that it is a variable and that the name of the variable is not the actual value.
+    */
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2 className="title">I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="Password"
+            required
+          />
+
+          <CustomButton type="submit"> Sign In </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
